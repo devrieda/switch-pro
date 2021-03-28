7f4892031d823f788bc9da7b9e164b442553cfd5
Derek DeVries <devrieda@gmail.com>
2021-03-28 16:47:17 -0600

    switch to storing pressed events in an object

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import SwitchPro from './lib/index.js'
 
 function handleChange(gamePad) {
-  if (gamePad.pressed.length === 0) {
+  const keys = Object.keys(gamePad.pressed)
+
+  if (keys.length === 0) {
     input.innerHTML = 'No button pressed at the moment...';
+
   } else {
-    input.innerHTML = gamePad.pressed.join(' + ');
+    const pressed = keys.map((key) => {
+      return `${key}: ${gamePad.pressed[key]}`
+    })
+    input.innerHTML = pressed.join(' + ');
   }
 }
 
 const switchPro = new SwitchPro(window)
 switchPro.addListener(handleChange)
-
-const pressed = {
-  "A": 1,
-  "B": 1,
-  "LJOY UP": .2,
-  "LJOY RIGHT": .8
-}
